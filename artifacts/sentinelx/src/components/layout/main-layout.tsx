471d196e27a00ed89a12dd0a6e49248122f8adb9
Aakash <aakash@merkisys.com>
2026-06-13 23:39:02 +0530

    update main layout

--- artifacts/sentinelx/src/components/layout/main-layout.tsx
+++ artifacts/sentinelx/src/components/layout/main-layout.tsx
@@ -1,4 +1,5 @@
 import { Sidebar } from "./sidebar";
+import { NeuralBackground } from "./neural-background";
 
 interface MainLayoutProps {
   children: React.ReactNode;
@@ -6,11 +7,13 @@ interface MainLayoutProps {
 
 export function MainLayout({ children }: MainLayoutProps) {
   return (
-    <div className="flex h-screen overflow-hidden bg-background">
+    <div className="flex h-screen overflow-hidden bg-background relative">
+      <NeuralBackground />
       <Sidebar />
-      <main className="flex-1 overflow-y-auto overflow-x-hidden">
+      <main className="flex-1 h-full overflow-y-auto overflow-x-hidden z-10 relative bg-transparent">
         {children}
       </main>
     </div>
   );
 }
+
